68c5d5378a61ae125be6f8ee1255b9d67fa28053
palmernickolas <nido5587@gmail.com>
2016-11-20 02:10:47 -0500

    MapActivity (zoom in to current location not working)
    Location services and permission requests are working, does not zoom to
    current location on startup

--- app/src/main/java/com/theprogrammingturkey/hackohio2016/MainActivity.java
+++ app/src/main/java/com/theprogrammingturkey/hackohio2016/MainActivity.java
@@ -1,7 +1,8 @@
 package com.theprogrammingturkey.hackohio2016;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+        }
+
 }
