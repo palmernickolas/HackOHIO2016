cbbd8e4752d135751910ca5bad7c3d73027b7205
Matthew Friedman <Matthew Friedman>
2016-11-19 19:14:37 -0500

    here da shiyt

--- app/src/main/java/com/theprogrammingturkey/hackohio2016/MainActivity.java
+++ app/src/main/java/com/theprogrammingturkey/hackohio2016/MainActivity.java
@@ -2,6 +2,8 @@ package com.theprogrammingturkey.hackohio2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v) {
+        if (v.getId() == R.id.transfer) {
+            EditText transferAm = (EditText) findViewById(R.id.amount);
+            int transferAmount = Integer.parseInt(transferAm.toString());
+
+
+
+        }
     }
 }
